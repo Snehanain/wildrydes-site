205870864bee23352051d784476ec4d3110be44e
Snehanain <134839248+Snehanain@users.noreply.github.com>
2025-03-27 23:41:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_qMeryV8BP', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2m34gt0o5tpd4bnue122bvh61t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_7Pr4KrAya', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'v6qo3n28gjnpamnij0jtvtlhf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
